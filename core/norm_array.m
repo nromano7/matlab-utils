9f76fa9b1001067428aa3ce432d17d79a8e9bce2
John DeVitis <johndevitis@gmail.com>
2016-11-29 14:40:46 -0500

    fixed bug in norm_array

--- core/norm_array.m
+++ core/norm_array.m
@@ -2,5 +2,5 @@ function out = norm_array(A)
 %% norm_array(in) -> normalize any size 2d array
 % author: john devitis
 % create date: 31-Oct-2016 18:50:07
-	out = bsxfun(@rdivide, A, max(A,[],1));	
+	out = bsxfun(@rdivide, A, max(abs(A),[],1));	
 end
