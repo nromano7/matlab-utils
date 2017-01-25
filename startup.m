56182dd6322f651b6bbd098405ef3141d985ce83
John DeVitis <johndevitis@gmail.com>
2017-01-25 10:30:00 -0500

    moved startup file

---
+++ startup.m
@@ -0,0 +1,30 @@
+function startup
+%% default matlab start up routine
+    fprintf('Hello, John. Its nice to see you again. \n');
+    fprintf('Ill go ahead and initialize some things for you.\n');
+
+    %% shuffle random numbers
+    fprintf('Shuffling random numbers... ');
+    rng('shuffle');
+    fprintf('Done.\n');
+    
+    %% add desired folders to matlab search path
+    
+    root = 'C:\Users\John\repos'; % paths to add
+    
+    % folders to add
+    folders = { 'matlab-utils', ...
+				'vma',...
+                };
+
+    fprintf('\tAdding paths:\n');
+    for ii = 1:length(folders)
+        fname = fullfile(root,folders{ii});
+        addpath(genpath(fname));
+        fprintf('\t\t%s\n',fname);
+    end
+    fprintf('\tDone. Enjoy\n');
+    
+	%% change working directory to root
+	cd(root);
+end
